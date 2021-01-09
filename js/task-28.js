385957256c67a2e7e233d9908bb72150b0e4c5dd
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-01-09 14:32:00 +0200

    tasks 21-29

--- js/task-28.js
+++ js/task-28.js
@@ -1,1 +1,13 @@
+const firstArray = [26, 94, 36, 18];
+const secondArray = [17, 61, 23];
+const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// Пиши код ниже этой строки
 
+const anyElementInFirstIsEven = firstArray.some((item) => item % 2 === 0);
+const anyElementInFirstIsOdd = firstArray.some((item) => item % 2 !== 0);
+
+const anyElementInSecondIsEven = secondArray.some((item) => item % 2 === 0);
+const anyElementInSecondIsOdd = secondArray.some((item) => item % 2 !== 0);
+
+const anyElementInThirdIsEven = thirdArray.some((item) => item % 2 === 0);
+const anyElementInThirdIsOdd = thirdArray.some((item) => item % 2 !== 0);
